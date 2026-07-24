9321084c2ac8ea47a41164762725851fffbd6725
Ilya Sitnikov <31476786+sitnikovik@users.noreply.github.com>
2026-07-24 07:25:13 +0300

    release: v1.0.4 (#19)
    Added `make reset-changelog` as part of `make setup` to ensure a consistent changelog state during project initialization.
    
    See CHANGELOG.md for full details.

--- make/setup.mk
+++ make/setup.mk
@@ -1,5 +1,5 @@
 .PHONY: setup
-setup: itsme githooks
+setup: itsme githooks reset-changelog
 
 # Install Git hooks for automatic testing and linting.
 .PHONY: githooks
@@ -11,3 +11,28 @@ githooks:
 # Replace all template substrings with developer credentials.
 itsme:
 	@sh ./scripts/itsme.sh
+
+.PHONY: reset-changelog
+reset-changelog:
+	@set -e;
+	@printf '%s\n' \
+		'# Changelog' \
+		'' \
+		'All notable changes to this project will be documented in this file.' \
+		'' \
+		'The format is based on [Keep a Changelog](https://keepachangelog.com/en/2.0.0/),' \
+		'and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).' \
+		'' \
+		'## [Unreleased]' \
+		'' \
+		'### Added' \
+ 		'' \
+ 		'### Changed' \
+ 		'' \
+ 		'### Deprecated' \
+ 		'' \
+ 		'### Removed' \
+ 		'' \
+ 		'### Fixed' \
+ 		'' \
+ 		'### Security' > CHANGELOG.md
